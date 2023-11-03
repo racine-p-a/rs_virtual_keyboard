7986b695024847ecff63e2cdc166ad6a65eaca81
Pierre-Alexandre Racine <pierrealexandreracine@gmail.com>
2023-11-03 20:23:29 +0100

    First steps

--- virtual_keyboard.js
+++ virtual_keyboard.js
@@ -1,4 +1,23 @@
 import init, { greet } from "./pkg/rs_virtual_keyboard.js";
+
+// Fonctionne
+/*
+document.getElementById('boutonTest').addEventListener('click', function () {
+    init().then(() => {
+        greet("test");
+    })
+});
+*/
+
+init().then(() => {
+    document.getElementById('boutonTest').addEventListener('click', function () {
+
+        greet(document.getElementById('charTables').value);
+    });
+});
+
+/*
 init().then(() => {
     greet("titi");
 });
+*/
